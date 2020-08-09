fe7797c2b21062cf6154111e58beb57dfff22cc9
JavierR <javier.reys@gmail.com>
2020-08-09 05:10:01 -0400

    Trabajando con columnas

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.purple,
+              child: Text('Chart'),
+              elevation: 7,
+            ),
+          ),
+          Card(
+            color: Colors.amber,
+            child: Text('LIST of TX'),
+          ),
+        ],
+      ),
+    );
+  }
+}
